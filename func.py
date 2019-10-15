1f283de70861633e42bc4b76976248de72d3dd27
dobrosun <mr.dobrogost02@mail.ru>
2019-10-15 17:09:23 +0300

    add some functions and

--- func.py
+++ func.py
@@ -3,7 +3,6 @@
 import numpy as np
 
 class Functions():
-
     def cos(self, x):
         return np.cos(x)
 
@@ -16,5 +15,11 @@ class Functions():
     def sqrt(self, x):
         assert x >= 0, "Ожидалось положительно число"
         return np.sqrt(x)
+
+    def lim1(self, x):
+        return np.sin(x) / float(x)
+
+    def lim2(self, x):
+        return np.power(1 + 1/float(x), x)
 
 f = Functions()
--- graphics.py
+++ graphics.py
@@ -7,18 +7,23 @@ from scipy.interpolate import make_interp_spline
 from func import f
 
 
-x = np.arange(-50, 50)
-y = np.array(list(map(f.double, x)))
+x = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+y = np.array([1.21, 1.20, 1.18, 1.17, 1.16, 1.14, 1.13, 1.11, 1.10, 1.09])
+z = np.array([1.70, 1.55, 1.44, 1.29, 1.26, 1.19, 1.13, 1.07, 1.02, 1.08])
+#y = np.array(list(map(f.lim2, x)))
 
+_, ax = plt.subplots()
+ax.grid()
 
+ax.set_xlabel('расстояние, см')
+ax.set_ylabel('период, с')
 
-x_smooth = np.linspace(x.min(), x.max(), 1000)
-
+x_smooth = np.linspace(x.min(), x.max(), 1000000)
 
 spl = make_interp_spline(x, y, k=3)
 y_smooth = spl(x_smooth)
 
-
-
-plt.plot(x_smooth, y_smooth)
+plt.plot(x, y, label="T_прям")
+plt.plot(x, z, label="Т_пер")
+plt.legend(loc='upper right')
 plt.show()
